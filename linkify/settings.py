612e568085f871e11dd9ecb4afa8a6347151e9e0
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-08-01 22:34:49 +0100

    Edit index.html

--- linkify/settings.py
+++ linkify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['linkify-tai1.onrender.com']
+ALLOWED_HOSTS = ['linkify-tai1.onrender.com', '127.0.0.1']
 
 
 #AUTH_USER_MODEL = 'user.CustomUser'
