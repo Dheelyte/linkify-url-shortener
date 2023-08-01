c4df0ff8990fc69b50e39abc0f2d1cbb9f3b76af
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-08-01 19:24:12 +0100

    Add env variable in settings

--- linkify/settings.py
+++ linkify/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rp%+4fp!rudihx-t_*v3ptzwis73ozluqt3g#z#3@)y$jr@%n6'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
