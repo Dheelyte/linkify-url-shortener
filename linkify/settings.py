3acbe409895480a14398a64e15244bb5f18aba29
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-08-01 22:06:32 +0100

    Add name to allowed hosts

--- linkify/settings.py
+++ linkify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['linkify-tai1.onrender.com']
 
 
 #AUTH_USER_MODEL = 'user.CustomUser'
